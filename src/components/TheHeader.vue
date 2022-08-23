39d1c457fe582ca683eb4d117b9ef05c4012fae2
Gabridg <gabri99.del@gmail.com>
2022-08-23 15:53:17 +0200

    added Jumbotron, started adding price on sale!

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,7 +5,7 @@
                 <div class="d-flex justify-content-between align-items-center">
                     <span>We have a 95% Successful Pass Rate!</span>
                     <span>
-                        <i class="fa-solid fa-phone-flip"></i>
+                        <i class="fa-solid fa-phone-flip p-end-1"></i>
                         give us a call to book your tuition!
                         1-800-555-555
                     </span>
@@ -14,19 +14,19 @@
         </section>
         <section class="navbar">
             <div class="container">
-                <div class="d-flex">
+                <div class="d-flex justify-content-between align-items-center w-100">
                     <div>
                         <img src="../assets/img/avada-drivers-logo-1x.png" alt="avada logo">
                     </div>
                     <div>
-                        <ul>
+                        <ul class="d-flex align-items-center">
                             <li><a href="#">HOME</a></li>
                             <li><a href="#">ABOUT</a></li>
                             <li><a href="#">PRICES</a></li>
                             <li><a href="#">COURSES</a></li>
                             <li><a href="#">LOCATIONS</a></li>
                             <li><a href="#">BLOG</a></li>
-                            <li><a href="#" class="btn btn-primary">BOOK NOW</a></li>
+                            <li><a href="#" class="btn btn-primary rounded-pill">BOOK NOW</a></li>
 
                         </ul>
                     </div>
@@ -34,8 +34,10 @@
             </div>
         </section>
         <section id="jumbotron">
-            <h1>Drive with Avada</h1>
-            <span>We offer the finest driving tuition money can buy</span>
+            <div class="container jumbo">
+                <h1 class="fw-bold">Drive with Avada</h1>
+                <span class="fs-4">We offer the finest driving tuition money can buy</span>
+            </div>
         </section>
     </div>
 </template>
@@ -53,6 +55,72 @@ export default {
 
 #alert-info {
     background-color: var(--grey);
-    color: var(--lightgrey)
+    color: var(--lightgrey);
+    padding: 5px;
+
+    span {
+        font-size: 12px;
+    }
+}
+
+ul {
+    list-style: none;
+    margin-bottom: 0;
+    filter: opacity(1);
+
+    li {
+        padding: 0 10px;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--white);
+    }
+
+    a:hover {
+        color: var(--green);
+        border-bottom: 2px solid var(--green);
+    }
+}
+
+.btn-primary {
+    background-color: var(--lightgreen);
+    border-color: var(--lightgreen);
+}
+
+.btn-primary:hover {
+    background-color: var(--green);
+    border-color: var(--green);
+    font-size: 20px;
+    font-weight: bold;
+}
+
+.navbar {
+    background-color: var(--lightdark);
+    filter: opacity(0.5);
+    position: absolute;
+    width: 100%;
+    padding: 20px 0;
+}
+
+#jumbotron {
+    background-image: url("../assets/img/homepage-hero-background.jpg");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 700px;
+    color: var(--white);
+}
+
+.jumbo {
+    padding-top: 15%;
+
+    h1 {
+        font-size: 60px;
+    }
+
+    span {
+        font-size: 40px;
+    }
 }
 </style>
