3a9a1895b5a36c11d521b21a7df1b0372dd34372
Gabridg <gabri99.del@gmail.com>
2022-08-24 16:56:20 +0200

    completed first section w/emit

---
+++ src/components/FormsCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="forms col-6">
+        <h2 class="m-3">First Lesson Free!</h2>
+        <span>When you make a block booking with us your first lesson is included free.</span>
+        <div class="input-control">
+            <input type="text" class="form-control p-3" placeholder="Your Name*" v-model.trim="userName">
+            <input type="email" class="form-control p-3" placeholder="Email*" v-model.trim="email">
+            <input type="text" class="form-control p-3" placeholder="Telephone" v-model.trim="telephone">
+            <input type="text" class="form-control p-3" placeholder="Location" v-model.trim="location">
+            <a href="#" class="btn btn-primary mt-4" @click="emitSave">REQUEST A CALLBACK</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FormsCard",
+    data() {
+        return {
+            userName: '',
+            email: '',
+            telephone: '',
+            location: '',
+        }
+    },
+    methods: {
+        emitSave() {
+            this.$emit('userInfo', this.userName, this.email, this.telephone, this.location)
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.forms {
+    text-align: center;
+    background-color: var(--white);
+    box-shadow: 5px 5px 25px var(--grey);
+    position: relative;
+    top: -150px;
+    height: 600px;
+    border-radius: 10px;
+
+    h2 {
+        color: var(--green);
+    }
+
+    span {
+        color: var(--secondarygrey);
+        margin: 20px 10px;
+        font-size: 20px;
+    }
+
+    input {
+        background-color: var(--lightgrey);
+        margin: 20px 10px;
+    }
+
+    input::placeholder {
+        color: var(--secondarygrey);
+    }
+
+    .input-control {
+        margin: 32px;
+    }
+}
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,13 +1,66 @@
 <template>
-    <div></div>
+    <div class="main">
+        <div class="container">
+            <section class="behind-wheel mt-5 row">
+                <div class="description col-6">
+                    <h2>Get Behind The Wheel</h2>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus eius quia iure voluptate quo
+                        cum
+                        nisi minus dignissimos ratione aliquam.
+                    </p>
+                    <span>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam facere laudantium veritatis
+                        accusantium quod hic, aut adipisci nulla at harum temporibus corrupti dolores esse voluptate
+                        sint
+                        tenetur sunt possimus rem?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur,
+                        reprehenderit id. Laborum, voluptates reiciendis ab sint, quaerat maxime magni ut ea nesciunt
+                        quam
+                        necessitatibus nisi nam. Molestias tempora eius nemo.
+                    </span>
+                </div>
+                <FormsCard @userInfo="userLogin"></FormsCard>
+            </section>
+        </div>
+        <section id="courses">
+
+        </section>
+    </div>
 </template>
 
 <script>
+import FormsCard from './FormsCard.vue';
 export default {
     name: "TheMain",
-
+    components: { FormsCard },
+    methods: {
+        userLogin() {
+            return alert('Hai effettuato il login!')
+        }
+    }
 }
 </script>
 
 <style lang="scss">
+.description {
+    margin: 0 auto;
+
+    h2 {
+        font-size: 45px;
+        color: var(--grey);
+        font-weight: 600;
+    }
+
+    p {
+        font-size: 25px;
+        color: var(--secondarygrey);
+        font-weight: 400;
+        margin: 20px 0;
+        line-height: 45px;
+    }
+
+    span {
+        color: var(--secondarygrey);
+    }
+}
 </style>
