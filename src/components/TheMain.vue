92ad40bf19fae3c2c74a90b6d3a2968861381718
Gabridg <gabri99.del@gmail.com>
2022-08-25 02:56:30 +0200

    elim props on card started 3th section

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -38,11 +38,66 @@
         </section>
         <section id="courses">
             <div class="container">
-                <div class="info-courses">
+                <div class="row">
+                    <div class="info-courses col-6 text-center">
+                        <h2 class="m-4">Courses</h2>
+                        <p class="m-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aliquam itaque accusamus iusto
+                            expedita quam illum ratione autem, est repellendus reiciendis omnis, nobis praesentium id
+                            quo aliquid veritatis delectus sequi!
+                        </p>
+                        <a href="#" class="btn btn-primary m-3 rounded-pill">COURSE INFORMATION</a>
 
+                    </div>
+                    <div class="school-info col-6 d-flex align-items-center">
+                        <div class="course-card">
+                            <img src="../assets/img/courses-passplus.jpg" alt="" class="img-fluid">
+                            <div class="course-txt">
+
+                                <h4>Pass Plus</h4>
+                                <a href="#" class="btn btn-secondary rounded-pill">LEARN MORE</a>
+                            </div>
+                        </div>
+                        <div class="course-card">
+                            <img src="../assets/img/course-intensive.jpg" alt="" class="img-fluid">
+                            <div class="course-txt">
+
+                                <h4>Intensive Course</h4>
+                                <a href="#" class="btn btn-secondary rounded-pill">LEARN MORE</a>
+                            </div>
+                        </div>
+                        <div class="course-card">
+                            <img src="../assets/img/courses-instructor.jpg" alt="" class="img-fluid">
+                            <div class="course-txt">
+                                <h4>Instructors</h4>
+                                <a href="#" class="btn btn-secondary rounded-pill">LEARN MORE</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="school-info">
-                    <Coursecard></Coursecard>
+            </div>
+        </section>
+        <section id="perc-rate">
+            <div class="container">
+                <div class="d-flex">
+                    <div class="rate-card">
+                        <div class="rate">
+                            95%
+                        </div>
+                        <h4>PASS RATE</h4>
+                    </div>
+                    <div class="rate-card">
+                        <div class="rate">
+                            100%
+                        </div>
+                        <h4>REFERRAL RATE</h4>
+                    </div>
+                    <div class="rate-card">
+                        <div class="rate">
+                            0%
+                        </div>
+                        <h4>ACCIDENT RATE</h4>
+                    </div>
                 </div>
             </div>
         </section>
@@ -51,21 +106,9 @@
 
 <script>
 import FormsCard from './FormsCard.vue';
-import Coursecard from './Coursecard.vue';
 export default {
     name: "TheMain",
-    components: { FormsCard, Coursecard },
-    data() {
-        return {
-            typeOfCours: [
-                {
-                    poster: '../assets/img/courses-passplus.jpg',
-                    title: 'Pass Plus'
-                }
-            ]
-
-        }
-    },
+    components: { FormsCard, },
     methods: {
         userLogin() {
             return alert('Hai effettuato il login!');
@@ -123,6 +166,98 @@ export default {
 
     .fa-phone-flip {
         margin-right: 35px;
+    }
+
+}
+
+#courses {
+    background-image: url(../assets/img/promise-background.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    .info-courses {
+        height: 450px;
+        border-radius: 20px;
+        box-shadow: 5px 5px 25px var(--grey);
+        width: 500px;
+        position: relative;
+        top: -30px;
+        background-color: var(--white);
+
+        h2 {
+            font-size: 50px;
+            font-weight: 600;
+            color: var(--grey);
+            padding: 20px;
+        }
+
+        p {
+            font-weight: 400;
+            color: var(--secondarygrey);
+            padding: 25px;
+            line-height: 35px;
+            font-size: 15px;
+        }
+
+        a {
+            color: var(--white);
+            font-weight: bold;
+            padding: 15px;
+            margin-top: 20px;
+        }
+
+
+    }
+
+    .school-info {
+        width: 770px;
+        margin-left: 50px;
+    }
+
+    .course-card {
+        text-align: center;
+        margin: 30px;
+
+        .course-txt {
+            margin-top: 30px;
+            color: var(--grey);
+
+            h4 {
+                font-weight: 600;
+            }
+
+            a {
+                background-color: transparent;
+                border: 2px solid var(--grey);
+                font-weight: 400;
+                color: var(--grey);
+                font-size: 13px;
+            }
+        }
+
+    }
+
+}
+
+.rate-card {
+    margin: 150px auto;
+    width: 400px;
+    height: 500;
+    text-align: center;
+    border-top: 5px solid var(--green);
+    border-radius: 20px;
+    box-shadow: 5px 5px 25px var(--grey);
+
+    .rate {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        border: 8px solid var(--green);
+        text-align: center;
+        line-height: 200px;
+        font-size: 50px;
+        margin: 40px auto;
     }
 }
 </style>
